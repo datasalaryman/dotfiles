f92f9c889dddb4b4e663e1b4cf2650effe1f6b79
Data Salaryman <datasalaryman@protonmail.com>
2026-03-02 19:26:17 +0800

    fix(nvim): update nvim and use native lsps

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,162 +1,408 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
--- -- bootstrap lazy.nvim, LazyVim and your plugins
--- require("config.lazy")
-
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
-vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
+vim.g.mapleader = " "
+
 vim.keymap.set('n', '<C-t>', '<cmd>tabnew<cr>')
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
+    "git", "clone", "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
+    "--branch=stable", lazypath,
   })
 end
-
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
+  -- Completion only
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "L3MON4D3/LuaSnip",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+    },
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = cmp.mapping.select_next_item(),
+          ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "path" },
+        }),
+        completion = { keyword_length = 1 },
+      })
+    end,
   },
-  {
-    "nvim-telescope/telescope.nvim",
-    tag ='0.1.5',
-    dependencies = { "nvim-lua/plenary.nvim" }
-  },
+
+  -- Treesitter (syntax highlighting)
   {
     "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate"
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+          "lua", "typescript", "javascript", "json", "python",
+          "terraform", "astro", "vue", "rust", "css", "zig"
+        },
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end,
   },
+
+  -- Telescope, theme, file browser, etc.
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = '0.1.5',
+    dependencies = { "nvim-lua/plenary.nvim" }
+  },
+  
   {
     "nvim-tree/nvim-tree.lua",
     version = "*",
     lazy = false,
     dependencies = { "nvim-tree/nvim-web-devicons" }
   },
-  -- {
-  --   'stevearc/oil.nvim',
-  --   opts = {},
-  --   -- Optional dependencies
-  --   dependencies = { "nvim-tree/nvim-web-devicons" },
-  -- },
+  
   {
     "nvim-telescope/telescope-file-browser.nvim",
     dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
-  }, 
-  -- {
-  --   "nvim-neo-tree/neo-tree.nvim",
-  --   branch = "v3.x",
-  --   dependencies = {
-  --     "nvim-lua/plenary.nvim",
-  --     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-  --     "MunifTanjim/nui.nvim",
-  --     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-  --   }
-  -- }
+  },
+  
   {
     "kawre/leetcode.nvim",
     build = ":TSUpdate html",
     dependencies = {
       "nvim-telescope/telescope.nvim",
-      "nvim-lua/plenary.nvim", -- required by telescope
+      "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
-
-        -- optional
       "nvim-treesitter/nvim-treesitter",
       "rcarriga/nvim-notify",
       "nvim-tree/nvim-web-devicons",
     },
-    opts = {
-        -- configuration goes here
-      ---@type lc.lang
-      lang = "python",
-    },
-  }, 
+    opts = { lang = "python" },
+  },
 }
-local opts = {}
 
-require("lazy").setup(plugins, opts)
+require("lazy").setup(plugins, {})
 
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<leader>ff', function() builtin.find_files({
-  find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" }
-}) end, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-require("telescope").load_extension "file_browser"
--- open file_browser with the path of the current buffer
-vim.api.nvim_set_keymap(
-  "n",
-  "<space>fb",
-  ":Telescope file_browser path=%:p:h select_buffer=true<CR>",
-  { noremap = true }
-)
+-- NATIVE LSP CONFIG (Neovim 0.11+)
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-local config = require("nvim-treesitter.configs")
-config.setup({
-  ensure_installed = { "lua", "typescript", "json", "javascript", "python", "terraform", "astro", "vue" },
-  highlight = { enable = true },
-  indent = { enable = true }
+-- TypeScript/JavaScript
+vim.lsp.config('ts_ls', {
+  cmd = { 'typescript-language-server', '--stdio' },
+  filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
+  root_markers = { 'tsconfig.json', 'jsconfig.json', 'package.json', '.git' },
+  capabilities = capabilities,
 })
 
--- require("nvim-tree").setup {}
--- require("nvim-tree").setup({
---   sort = {
---     sorter = "case_sensitive",
---   },
---   view = {
---     width = 30,
---   },
---   renderer = {
---     group_empty = true,
---   },
---   filters = {
---     dotfiles = false,
---   },
--- })
--- require("oil").setup({
---   default_file_explorer = true,
---   delete_to_trash = true,
---   view_options = {
---     -- Show files and directories that start with "."
---     show_hidden = true,
---     -- This function defines what is considered a "hidden" file
---     is_hidden_file = function(name, bufnr)
---       return vim.startswith(name, ".")
---     end,
---     -- This function defines what will never be shown, even when `show_hidden` is set
---     is_always_hidden = function(name, bufnr)
---       return false
---     end,
---     sort = {
---       -- sort order can be "asc" or "desc"
---       -- see :help oil-columns to see which columns are sortable
---       { "type", "asc" },
---       { "name", "asc" },
---     },
---   }
--- })
+-- CSS
+vim.lsp.config('cssls', {
+  cmd = { 'vscode-css-language-server', '--stdio' },
+  filetypes = { 'css', 'scss', 'less' },
+  root_markers = { 'package.json', '.git' },
+  capabilities = capabilities,
+})
 
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin"
+-- JSON
+vim.lsp.config('jsonls', {
+  cmd = { 'vscode-json-language-server', '--stdio' },
+  filetypes = { 'json', 'jsonc' },
+  root_markers = { '.git' },
+  capabilities = capabilities,
+})
 
-vim.api.nvim_set_keymap(
-  "n",
-  "<space>fb",
+-- Lua
+vim.lsp.config('lua_ls', {
+  cmd = { 'lua-language-server' },
+  filetypes = { 'lua' },
+  root_markers = { '.luarc.json', '.luarc.jsonc', '.git' },
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      diagnostics = { globals = { 'vim' } },
+      workspace = { library = vim.api.nvim_get_runtime_file('', true) },
+    },
+  },
+})
+
+-- Python
+vim.lsp.config('pyright', {
+  cmd = { 'pyright-langserver', '--stdio' },
+  filetypes = { 'python' },
+  root_markers = { 'pyproject.toml', 'setup.py', 'setup.cfg', 'requirements.txt', '.git' },
+  capabilities = capabilities,
+})
+
+-- Terraform
+vim.lsp.config('terraformls', {
+  cmd = { 'terraform-ls', 'serve' },
+  filetypes = { 'terraform', 'terraform-vars', 'hcl' },
+  root_markers = { '.terraform', '.git' },
+  capabilities = capabilities,
+})
+
+-- Astro
+vim.lsp.config('astro', {
+  cmd = { 'astro-ls', '--stdio' },
+  filetypes = { 'astro' },
+  root_markers = { 'package.json', '.git' },
+  capabilities = capabilities,
+})
+
+-- Vue
+vim.lsp.config('volar', {
+  cmd = { 'vue-language-server', '--stdio' },
+  filetypes = { 'vue' },
+  root_markers = { 'package.json', '.git' },
+  capabilities = capabilities,
+})
+
+-- Zig
+vim.lsp.config('zls', {
+  cmd = { 'zls' },
+  filetypes = { 'zig', 'zir' },
+  root_markers = { 'zls.json', '.git' },
+  capabilities = capabilities,
+})
+
+-- Rust
+vim.lsp.config('rust_analyzer', {
+  cmd = { 'rust-analyzer' },
+  filetypes = { 'rust' },
+  root_markers = { 'Cargo.toml', '.git' },
+  capabilities = capabilities,
+  settings = {
+    ['rust-analyzer'] = {
+      cargo = { allFeatures = true },
+      procMacro = { enable = true },
+    },
+  },
+})
+
+-- Enable all LSPs
+vim.lsp.enable({
+  'ts_ls', 'cssls', 'jsonls', 'lua_ls', 'pyright',
+  'terraformls', 'astro', 'volar', 'zls', 'rust_analyzer'
+})
+
+-- Keymaps
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Go to references" })
+vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover docs" })
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename" })
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
+
+-- Telescope
+local builtin = require("telescope.builtin")
+vim.keymap.set('n', '<leader>ff', function()
+  builtin.find_files({ find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" } })
+end, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+require("telescope").load_extension("file_browser")
+
+vim.api.nvim_set_keymap("n", "<space>fb",
   ":Telescope file_browser path=%:p:h select_buffer=true<CR>",
-  { noremap = true }
-)
+  { noremap = true })
+
+-- Theme
+require("catppuccin").setup()
+vim.cmd.colorscheme("catppuccin")
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set number")
+vim.g.mapleader = " "
+
+vim.keymap.set('n', '<C-t>', '<cmd>tabnew<cr>')
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+  -- LSP Config (simplifies server setup)
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      -- Servers with default config
+      local servers = {
+        "ts_ls", "cssls", "jsonls", "pyright",
+        "terraformls", "astro", "volar", "zls"
+      }
+      
+      for _, server in ipairs(servers) do
+        lspconfig[server].setup({ capabilities = capabilities })
+      end
+
+      -- Lua (needs special config for vim globals)
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+        settings = {
+          Lua = {
+            diagnostics = { globals = { "vim" } },
+            workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+          },
+        },
+      })
+
+      -- Rust
+      lspconfig.rust_analyzer.setup({
+        capabilities = capabilities,
+        settings = {
+          ["rust-analyzer"] = {
+            cargo = { allFeatures = true },
+            procMacro = { enable = true },
+          },
+        },
+      })
+    end,
+  },
+
+  -- Completion
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "L3MON4D3/LuaSnip",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+    },
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = cmp.mapping.select_next_item(),
+          ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "path" },
+        }),
+        completion = { keyword_length = 1 },
+      })
+    end,
+  },
+
+  -- Treesitter (syntax highlighting)
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+          "lua", "typescript", "javascript", "json", "python",
+          "terraform", "astro", "vue", "rust", "css", "zig"
+        },
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end,
+  },
+
+  -- Telescope, theme, file browser, etc.
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = '0.1.5',
+    dependencies = { "nvim-lua/plenary.nvim" }
+  },
+  
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = { "nvim-tree/nvim-web-devicons" }
+  },
+  
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
+  },
+  
+  {
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "rcarriga/nvim-notify",
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = { lang = "python" },
+  },
+}
+
+require("lazy").setup(plugins, {})
+
+-- Keymaps
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Go to references" })
+vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover docs" })
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename" })
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
+
+-- Telescope
+local builtin = require("telescope.builtin")
+vim.keymap.set('n', '<leader>ff', function()
+  builtin.find_files({ find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" } })
+end, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+require("telescope").load_extension("file_browser")
+
+vim.api.nvim_set_keymap("n", "<space>fb",
+  ":Telescope file_browser path=%:p:h select_buffer=true<CR>",
+  { noremap = true })
+
+-- Theme
+require("catppuccin").setup()
+vim.cmd.colorscheme("catppuccin")
