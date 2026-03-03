bea13bcb1d4987338d985210fbecfcc1bcb4bbbb
Data Salaryman <datasalaryman@protonmail.com>
2026-03-04 00:58:27 +0800

    fix(newsboat): do override

--- newsboat/link.sh
+++ newsboat/link.sh
@@ -4,39 +4,25 @@ set -o errexit
 set -o nounset
 set -o pipefail
 if [[ "${TRACE-0}" == "1" ]]; then
-    set -o xtrace
+	set -o xtrace
 fi
 
 if [[ "${1-}" =~ ^-*h(elp)?$ ]]; then
-    echo 'Usage: ./link.sh
+	echo 'Usage: ./link.sh
 
 Symlink the newsboat urls to this github repository
 
 '
-    exit
+	exit
 fi
 
 main() {
-  if [[ ! -d "$HOME/.newsboat" ]]
-    then
-      mkdir "$HOME/.newsboat"
-      ln -s "$PWD/newsboat/urls" "$HOME/.newsboat/urls"
-  fi
+	if [[ ! -d "$HOME/.newsboat" ]]; then
+		mkdir -p "$HOME/.newsboat"
+	fi
 
-  if [[ ! -f "$HOME/.newsboat/urls" ]]
-    then
-      ln -s "$PWD/newsboat/urls" "$HOME/.newsboat/urls"
-    else
-      rm "$HOME/.newsboat/urls"
-      ln -s "$PWD/newsboat/urls" "$HOME/.newsboat/urls"
-  fi
-  if [[ ! -f "$HOME/.newsboat/config" ]]
-    then
-      ln -s "$PWD/newsboat/config" "$HOME/.newsboat/config"
-    else
-      rm "$HOME/.newsboat/config"
-      ln -s "$PWD/newsboat/config" "$HOME/.newsboat/config"
-  fi
+	ln -sf "$PWD/urls" "$HOME/.newsboat/urls"
+	ln -sf "$PWD/config" "$HOME/.newsboat/config"
 }
 
 main "$@"
